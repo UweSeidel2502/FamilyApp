41d2942e51c8511ff743778555c91e44f2b6f69c
UweSeidel2502 <uwe.seidel@fn.de>
2024-10-07 15:56:18 +0200

    Fenster für Handy

---
+++ UI/Handy/frmHandy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FamilyApp.UI.Handy
+{
+    public partial class frmHandy : Form
+    {
+        public frmHandy()
+        {
+            InitializeComponent();
+        }
+    }
+}
